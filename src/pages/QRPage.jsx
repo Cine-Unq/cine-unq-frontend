5a4ae1e2eda15995722b2ef5b90559f37ff64887
miguelEq <miguelequilea18@gmail.com>
2023-06-29 21:53:36 -0300

    agrego informacion en la vista de compra y cambio font family

--- src/pages/QRPage.jsx
+++ src/pages/QRPage.jsx
@@ -24,15 +24,24 @@ export default function QRPage() {
             { purchase ? 
                 <> 
                     <NavBar isAdmin={false}></NavBar>
-                    <h4 style={{color: 'white', display: 'flex', justifyContent: 'center'}}>Se finalizo la compra exitosamente</h4>
-                    <div style={{display:'flex', justifyContent: 'center'}}>
-                        <div style={{ display:'flex', justifyContent: 'center', background: 'white', padding: 10, width: '300px'}}>
-                            <QRCode 
-                                data-testid='qr-compra'
-                                size={256}
-                                value={JSON.stringify({compra:idCompra})}
-                                viewBox={`0 0 256 256`}
-                            />
+                    <h3 style={{color: 'white', display: 'flex', justifyContent: 'center'}}>Se finalizó la compra exitosamente</h3>
+                    <br></br>
+                    <div style={{display:'grid', justifyContent: 'center', color: "white"}}>
+                        <div style={{textAlign:'center'}}>
+                            <h4>Sala : {purchase.sala}</h4>   
+                            <h4>Pelicula : {purchase.pelicula}</h4>   
+                            <h4>Horario función : {purchase.funcion}</h4>
+                            <h4>Cantidad de asientos comprados : {purchase.asientosReservados.length}</h4>   
+                        </div>
+                        <div style={{display:'grid', justifyContent: 'center'}}>
+                            <div style={{ display:'flex', justifyContent: 'center', background: 'white', padding: 10, width: '300px'}}>
+                                <QRCode 
+                                    data-testid='qr-compra'
+                                    size={256}
+                                    value={JSON.stringify({compra:idCompra})}
+                                    viewBox={`0 0 256 256`}
+                                />
+                            </div>
                         </div>
                     </div>
                 </>
