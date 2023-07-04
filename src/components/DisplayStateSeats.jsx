8452afccc0e78944bddf7cfb71bb769770b34423
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2023-07-04 14:19:20 -0300

    cambio a algunos colores

--- src/components/DisplayStateSeats.jsx
+++ src/components/DisplayStateSeats.jsx
@@ -9,33 +9,33 @@ export default function DisplayStateSeats({ info }) {
             })
     }, [])
 
-    const createMatrixWithSeats = ({cantColumnas, cantFilas, asientos}) => {
+    const createMatrixWithSeats = ({ cantColumnas, cantFilas, asientos }) => {
         let matrix = [];
-        for(let f=0; f < cantFilas; f++) {
+        for (let f = 0; f < cantFilas; f++) {
             matrix.push([]);
-            for(let c=0; c < cantColumnas; c++) {
+            for (let c = 0; c < cantColumnas; c++) {
                 matrix[f][c] = {
-                    "posFila":f,
-                    "posColumna":c,
+                    "posFila": f,
+                    "posColumna": c,
                     "estado": "NODISPONIBLE",
                     "selected": false
                 };
             }
         }
         asientos.forEach(a => {
-            matrix[a.posFila][a.posColumna] = {...a,selected:false};
+            matrix[a.posFila][a.posColumna] = { ...a, selected: false };
         });
         return matrix;
     }
 
     const colorOfStateSeat = (estado) => {
         if (estado === "LIBRE")
-            return { background: "white" }    
+            return { background: "white" }
         if (estado === "RESERVADO")
-            return { background: "yellow" }    
+            return { background: "yellow" }
         if (estado === "OCUPADO")
-            return { background: "green"};
-        return { background: "black"};
+            return { background: "green" };
+        return { background: "black" };
 
     }
     const renderSeats = () => {
@@ -52,40 +52,40 @@ export default function DisplayStateSeats({ info }) {
         return (
             <div style={layout} >
                 {
-                    seats.flat(1).map((seat,index) => {
+                    seats.flat(1).map((seat, index) => {
                         const style = colorOfStateSeat(seat.estado);
                         return (
                             <div className="seat" key={index} style={style}>
                             </div>
                         )
 
-                            
-                    })               
+
+                    })
                 }
             </div>
         )
     }
-            
+
     return (
         <div>
             <div className="d-flex justify-content-center">
                 <ul className="confirmacion">
                     <li>
-                        <span className="seat" style={{background:'white'}}/> <small>Sin reservar</small>
+                        <span className="seat" style={{ background: 'white' }} /> <small>Sin reservar</small>
                     </li>
                     <li>
-                        <span className="seat" style={{background:'yellow'}}/> <small>Reservado</small>
+                        <span className="seat" style={{ background: 'yellow' }} /> <small>Reservado</small>
                     </li>
                     <li>
-                        <span className="seat" style={{background:'green'}}/> <small>Confirmado</small>
+                        <span className="seat" style={{ background: 'green' }} /> <small>Confirmado</small>
                     </li>
                 </ul>
             </div>
-           
+
             <div className="Cinema">
                 <div className="screen" data-testid='estado-asientos' />
                 {
-                    renderSeats() 
+                    renderSeats()
                 }
             </div>
         </div>
--- src/components/ListSeatsReserved.jsx
+++ src/components/ListSeatsReserved.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import Card from 'react-bootstrap/Card';
 import Seat from '../assets/seat-cinema.png';
 import Button from 'react-bootstrap/Button';
@@ -18,23 +19,25 @@ export default function ListSeatsReserved() {
             then(res => {
                 setPurchase(res);
                 const asientos = res.asientosOcupados.concat(res.asientosReservados);
-                const seatsWithStyle  = addStyleToSeat(asientos)
+                const seatsWithStyle = addStyleToSeat(asientos)
                 setSeats(seatsWithStyle)
-            })}, []);
+            })
+    }, []);
     useEffect(() => {
         getPurchase(idCompra).
             then(res => {
                 setPurchase(res);
                 const asientos = res.asientosOcupados.concat(res.asientosReservados);
-                const seatsWithStyle  = addStyleToSeat(asientos)
+                const seatsWithStyle = addStyleToSeat(asientos)
                 setSeats(seatsWithStyle)
-            })}, [refresh]);
-            
+            })
+    }, [refresh]);
+
     const addStyleToSeat = (seats) => {
         const res = seats.map(a => {
-            let obj = {...a};
+            let obj = { ...a };
             if (a.estado === 'OCUPADO') {
-                obj.style = 'green';
+                obj.style = '#2cef2c'; //Verde
             } else {
                 obj.style = 'white';
             }
@@ -46,25 +49,25 @@ export default function ListSeatsReserved() {
         if (seat.estado === 'OCUPADO') {
             return
         }
-        const updatesSeats = seats.map( s => {
+        const updatesSeats = seats.map(s => {
             let copy = null;
             if (seat.id === s.id) {
                 switch (seat.estado) {
-                case 'RESERVADO' :
-                    copy = {...s}; 
-                    copy.estado = 'SELECTED';
-                    copy.style =  'blue';
-                    break;
-                case 'SELECTED' : 
-                    copy = {...s};
-                    copy.estado = 'RESERVADO';
-                    copy.style = 'white'
-                    break;    
+                    case 'RESERVADO':
+                        copy = { ...s };
+                        copy.estado = 'SELECTED';
+                        copy.style = '#FFFF00'; //Amarillo
+                        break;
+                    case 'SELECTED':
+                        copy = { ...s };
+                        copy.estado = 'RESERVADO';
+                        copy.style = 'white'
+                        break;
                 }
             }
-            return copy != null ? copy : s  
+            return copy != null ? copy : s
         })
-        setSeats(updatesSeats) 
+        setSeats(updatesSeats)
     }
     const handleModal = () => {
         setSeatsSelected(seats.filter(s => s.estado === 'SELECTED'))
@@ -76,47 +79,47 @@ export default function ListSeatsReserved() {
         setRefresh(!refresh)
     }
     return (
-        <div style={{color: 'white'}}>
-            {modal ? <ModalConfirmSeat idCompra={idCompra} idCliente={purchase.clienteID} seats={seatsSelected} onClose={()=> setModal(false)} refresh={handleRefresh} />: <></>}
+        <div style={{ color: 'white' }}>
+            {modal ? <ModalConfirmSeat idCompra={idCompra} idCliente={purchase.clienteID} seats={seatsSelected} onClose={() => setModal(false)} refresh={handleRefresh} /> : <></>}
             {
                 purchase && seats &&
-            <>
-                <p></p>
-                <h2 style={{textAlign: 'center'}}>Película : {purchase.pelicula}</h2>
-                <h2 style={{textAlign: 'center'}}>Función : {purchase.funcion}</h2>
-                <h2 style={{textAlign: 'center'}}>Asientos reservados :</h2>
-                <div className="d-flex justify-content-center">
-                    <ul className="confirmacion">
-                        <li>
-                            <span className="seat" style={{background:'white'}}/> <small>Sin confirmar</small>
-                        </li>
-                        <li>
-                            <span className="seat" style={{background:'green'}}/> <small>Confirmado</small>
-                        </li>
-                        <li>
-                            <span className="seat" style={{background:'blue'}}/> <small>Seleccionado</small>
-                        </li>
-                    </ul>
-                </div>
-                <div className="d-flex justify-content-center">
-                    <div style={{width:200, color:'Black'}}>
-                        {seats.map(asiento => {
-                            return (
-                                <Card data-testid='asiento-info' key={asiento.id} style={{background: asiento.style, marginBottom:10}} onClick={() => selectSeat(asiento)}>
-                                    <Card.Img variant="top" src={Seat} />
-                                    <Card.Body>
-                                        <Card.Title>Asiento {`${asiento.letter}${asiento.posColumna + 1}`} </Card.Title>
-                                    </Card.Body>
-                                </Card>
-                            )
-                        }) }
+                <>
+                    <p></p>
+                    <h2 style={{ textAlign: 'center' }}>Película : {purchase.pelicula}</h2>
+                    <h2 style={{ textAlign: 'center' }}>Función : {purchase.funcion}</h2>
+                    <h2 style={{ textAlign: 'center' }}>Asientos reservados :</h2>
+                    <div className="d-flex justify-content-center">
+                        <ul className="confirmacion">
+                            <li>
+                                <span className="seat" style={{ background: 'white' }} /> <small>Sin confirmar</small>
+                            </li>
+                            <li>
+                                <span className="seat" style={{ background: '#2cef2c' }} /> <small>Confirmado</small>
+                            </li>
+                            <li>
+                                <span className="seat" style={{ background: '#FFFF00' }} /> <small>Seleccionado</small>
+                            </li>
+                        </ul>
                     </div>
-                </div>
-                {<p data-testid='cant-asientos-seleccionados-de-compra' className="d-flex justify-content-center">Asientos seleccionados {seats.filter(s => s.estado === 'SELECTED').length}</p>}
-                <div className="d-flex justify-content-center">
-                    <Button data-testid='confirmar-asientos' variant="secondary" onClick={handleModal}> Confirmar asientos</Button>
-                </div>
-            </>}
+                    <div className="d-flex justify-content-center">
+                        <div style={{ width: 200, color: 'Black' }}>
+                            {seats.map(asiento => {
+                                return (
+                                    <Card data-testid='asiento-info' key={asiento.id} style={{ background: asiento.style, marginBottom: 10 }} onClick={() => selectSeat(asiento)}>
+                                        <Card.Img variant="top" src={Seat} />
+                                        <Card.Body>
+                                            <Card.Title>Asiento {`${asiento.letter}${asiento.posColumna + 1}`} </Card.Title>
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    {<p data-testid='cant-asientos-seleccionados-de-compra' className="d-flex justify-content-center">Asientos seleccionados {seats.filter(s => s.estado === 'SELECTED').length}</p>}
+                    <div className="d-flex justify-content-center">
+                        <Button data-testid='confirmar-asientos' variant="secondary" onClick={handleModal}> Confirmar asientos</Button>
+                    </div>
+                </>}
         </div>
     )
 }
